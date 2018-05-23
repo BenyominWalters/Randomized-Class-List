82c653d5bc93fca2b8478e54791598d1bdc0ebdc
Benyomin Walters <31862759+BenyominWalters@users.noreply.github.com>
2018-05-22 23:01:22 -0500

    Added program from PC

---
+++ Mendy's Ramdomized Class List.py
@@ -0,0 +1,10 @@
+# Mendy's Ramdomized Class List
+import random
+classlist = ["Yudel", "Shimon", "Eli Nosson", "Mendy",
+             "Shalom", "Levi S.", "Yehoshua Dovid", "Levi B.",
+             "Nochum", "Mendel R.", "Shalom Dovber", "Eli", "Mendel S.",
+             "Yochanan", "Dovi", "Zalman G.", "Levi Simcha", "Mottel"]
+
+random.shuffle(classlist)
+
+print(classlist)
